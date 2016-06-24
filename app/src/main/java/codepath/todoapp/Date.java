66bcf32fba80a5255e3ec301756965ed60d23dd0
Andrews, John L <John.L.Andrews@disney.com>
2016-06-24 11:00:34 -0700

    got intetns working

--- app/src/main/java/codepath/todoapp/Date.java
+++ app/src/main/java/codepath/todoapp/Date.java
@@ -1,10 +1,14 @@
 package codepath.todoapp;
 
+import java.io.Serializable;
+
 /**
  * Created by andrj148 on 6/23/16.
  */
-public class Date {
+public class Date implements Serializable{
     public Integer year;
     public Integer month;
     public Integer day;
+
+
 }
--- app/src/main/java/codepath/todoapp/MainActivity.java
+++ app/src/main/java/codepath/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package codepath.todoapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -85,7 +86,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void populateArrayItems() {
-        readItems();
+//        readItems();
+        createTestItemToAddToArrayList();
         ArrayList<String> tempNameArray = retrieveStringsFromItemObjects(TASK_NAME);
         aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tempNameArray);
 
@@ -162,14 +164,14 @@ public class MainActivity extends AppCompatActivity {
     }
     //endregion
 
-    public void navigateToEditActivity(int index) {
+    public void navigateToEditActivity(int requestCode) {
         Intent transitionToEdit = new Intent(MainActivity.this, EditItemActivity.class);
 
-        if (index == EDITED_ITEM_REQUEST_CODE) {
-            transitionToEdit.putExtra("Item", todoItems.get(selectedIndexRow));
+        if (requestCode == EDITED_ITEM_REQUEST_CODE) {
+            transitionToEdit.putExtra(EditItemActivity.SELECTED_ITEM, todoItems.get(selectedIndexRow));
+            Log.d("MainActivity", "inserted selected row");
         }
-        startActivity(transitionToEdit);
-//        startActivityForResult(transitionToEdit, index);
+        startActivityForResult(transitionToEdit, requestCode);
     }
 
 
