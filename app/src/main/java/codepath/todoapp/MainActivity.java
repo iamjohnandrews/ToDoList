884b92ffe31ff9a744575d6c4e712e4ea3152f28
Andrews, John L <John.L.Andrews@disney.com>
2016-06-24 05:44:48 -0700

    add button to ActionBar

--- app/src/main/java/codepath/todoapp/MainActivity.java
+++ app/src/main/java/codepath/todoapp/MainActivity.java
@@ -3,6 +3,8 @@ package codepath.todoapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -57,6 +59,14 @@ public class MainActivity extends AppCompatActivity {
                 navigateToEditActivity(position);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar, menu);
+        return true;
     }
 
     public void populateArrayItems() {
